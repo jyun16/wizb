574cea3c8e8d9d59d586dc772a80d00d3b0d9d64
JN <jyun16@gmail.com>
2026-05-01 15:53:29 +0900

    user

--- src/model/user.js
+++ src/model/user.js
@@ -11,11 +11,14 @@ class Self extends Model {
 	ignoreFields() {
 		return [ 'password' ].concat(super.ignoreFields())
 	}
+	async select(w={}, fields='*', filter=[], asArray=false) {
+		const ret = await super.select(w, fields, filter, asArray)
+		if (ret) { ret.forEach(v => v.stash = parseJSON(v.stash) || '{}') }
+		return ret
+	}
 	async one(w) {
 		const ret = await super.one(w)
-		if (ret) {
-			ret.stash = parseJSON(ret.stash)
-		}
+		if (ret) { ret.stash = parseJSON(ret.stash || '{}') }
 		return ret
 	}
 	async insert(d) {
